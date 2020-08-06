87dd8380cfd6d93d2962c2f3b030c08eb0a2d36d
KarimASC20 <62969870+KarimASC20@users.noreply.github.com>
2020-08-06 11:45:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const usernameElement = document.getElementById("username");
+const messageElement = document.getElementById("message");
+const button = document.getElementById("submitButton");
+button.addEventListener("click",updateDB);
+
+//Set database object here
+let db = firebase.database().ref();
+
+/**
+ * Updates the database with the username and message.
+ */
+function updateDB(event){
+    event.preventDefault();
+    const username        = usernameElement.value;
+    const message         = messageElement.value;
+
+    usernameElement.value = "";
+    messageElement.value  = "";
+
+    console.log(username + " : " + message);
+
+    //Update database here
+    let value= {
+        NAME: username,
+        MESSAGE: message
+    };
+
+    db.push(value);
+
+}
+
+// Set database "child_added" event listener here
+db.on("child_added")
